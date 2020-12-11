c26c41db298e8bc07d877ab8277b1630a67be905
oddlyspaced <oddlyspaced1@gmail.com>
2020-12-12 01:36:58 +0530

    EditorActivity: Set sample background and make status bar transparent

--- app/src/main/java/com/oddlyspaced/prjkt/EditorActivity.kt
+++ app/src/main/java/com/oddlyspaced/prjkt/EditorActivity.kt
@@ -1,7 +1,10 @@
 package com.oddlyspaced.prjkt
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import com.oddlyspaced.prjkt.databinding.ActivityEditorBinding
 
 class EditorActivity : AppCompatActivity() {
@@ -12,6 +15,13 @@ class EditorActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEditorBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        this.window.apply {
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            statusBarColor = Color.TRANSPARENT
+        }
 
         supportFragmentManager.beginTransaction().add(binding.frag.id, ShapeEditorFragment.newInstance(binding.textView), "hi").commit()
     }
