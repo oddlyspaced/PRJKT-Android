7c2bada5e7dfb1d8a2cb1fe2292df2bb233e94a6
oddlyspaced <oddlyspaced1@gmail.com>
2020-12-14 03:08:41 +0530

    EditorActivity: Test color picker gradient

--- app/src/main/java/com/oddlyspaced/prjkt/activity/EditorActivity.kt
+++ app/src/main/java/com/oddlyspaced/prjkt/activity/EditorActivity.kt
@@ -2,15 +2,14 @@ package com.oddlyspaced.prjkt.activity
 
 import android.graphics.*
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.WindowManager
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.*
+import androidx.core.graphics.alpha
+import androidx.core.graphics.blue
 import androidx.core.graphics.drawable.toBitmap
-import com.oddlyspaced.prjkt.R
+import androidx.core.graphics.green
+import androidx.core.graphics.red
 import com.oddlyspaced.prjkt.databinding.ActivityEditorBinding
 import com.oddlyspaced.prjkt.fragment.DesignEditorFragment
 import com.oddlyspaced.prjkt.fragment.MoveEditorFragment
@@ -39,6 +38,7 @@ class EditorActivity : AppCompatActivity() {
         }
 
         // overlayDrawable()
+        colorTest()
 
         shapeEditorFragment = ShapeEditorFragment.newInstance(binding.imgIconBackground)
         resizeEditorFragment = ResizeEditorFragment.newInstance(binding.imgIconBackground)
@@ -69,6 +69,32 @@ class EditorActivity : AppCompatActivity() {
             supportFragmentManager.popBackStack()
         }
 
+    }
+
+    private fun colorTest() {
+        val sourceOriginal = binding.imageView6.drawable.toBitmap()
+        val newLayer = Bitmap.createBitmap(sourceOriginal.height, sourceOriginal.width, Bitmap.Config.ARGB_8888)
+        val paint = Paint()
+        val luar = LinearGradient(0F, 0F, 0F, newLayer.height.toFloat(), Color.WHITE, Color.BLACK, Shader.TileMode.CLAMP)
+        val rgb = Color.HSVToColor(floatArrayOf(1f, 1f, 1f))
+        val dalam = LinearGradient(0F, 0F, newLayer.width.toFloat(), 0F, Color.WHITE, rgb, Shader.TileMode.CLAMP)
+        val shader = ComposeShader(luar, dalam, PorterDuff.Mode.MULTIPLY)
+        paint.shader = shader
+        val canvas = Canvas(newLayer)
+        canvas.drawRect(0F, 0F, newLayer.width.toFloat(), newLayer.height.toFloat(), paint)
+        binding.imageView6.setImageBitmap(newLayer)
+
+        /*
+        if (paint == null) {
+			paint = new Paint();
+			luar = new LinearGradient(0.f, 0.f, 0.f, this.getMeasuredHeight(), 0xffffffff, 0xff000000, TileMode.CLAMP);
+		}
+		int rgb = Color.HSVToColor(color);
+		Shader dalam = new LinearGradient(0.f, 0.f, this.getMeasuredWidth(), 0.f, 0xffffffff, rgb, TileMode.CLAMP);
+		ComposeShader shader = new ComposeShader(luar, dalam, PorterDuff.Mode.MULTIPLY);
+		paint.setShader(shader);
+		canvas.drawRect(0.f, 0.f, this.getMeasuredWidth(), this.getMeasuredHeight(), paint);
+         */
     }
 
     private fun overlayDrawable() {
