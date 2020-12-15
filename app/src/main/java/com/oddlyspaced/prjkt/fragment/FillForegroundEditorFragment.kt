251bcc907ab965ab310e912b585285447e4a556b
oddlyspaced <oddlyspaced1@gmail.com>
2020-12-15 23:23:15 +0530

    FillForegroundEditorFragment: Setup base

---
+++ app/src/main/java/com/oddlyspaced/prjkt/fragment/FillForegroundEditorFragment.kt
@@ -0,0 +1,33 @@
+package com.oddlyspaced.prjkt.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import com.oddlyspaced.prjkt.databinding.FragmentFillForegroundBinding
+
+class FillForegroundEditorFragment(val root: Int, val foreground: ImageView) : Fragment() {
+
+    companion object {
+        fun newInstance(root: Int, img: ImageView): FillForegroundEditorFragment {
+            return FillForegroundEditorFragment(root, img)
+        }
+    }
+
+    private lateinit var binding: FragmentFillForegroundBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentFillForegroundBinding.inflate(layoutInflater, container, false)
+
+        binding.cvFillForegroundColor.setOnClickListener {
+            fragmentManager?.beginTransaction()?.addToBackStack("move")?.add(root, ColorPickerFragment.newInstance { color ->
+                foreground.setColorFilter(color)
+            }, "tagColorPicker")?.commit()
+        }
+
+        return binding.root
+    }
+
+}
