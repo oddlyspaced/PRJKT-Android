a02933ded5b8714eccd9ac1ec9d122a7d2788d8d
oddlyspaced <oddlyspaced1@gmail.com>
2020-12-17 00:01:30 +0530

    IconProperties: Init

---
+++ app/src/main/java/com/oddlyspaced/prjkt/modal/IconProperties.kt
@@ -0,0 +1,17 @@
+package com.oddlyspaced.prjkt.modal
+
+data class IconProperties(
+    var backgroundRadius: Float,
+    var backgroundSides: Int,
+    var backgroundRotation: Int,
+    var backgroundWidth: Float,
+    var backgroundHeight: Float,
+    var backgroundStartColor: String,
+    var backgroundEndColor: String,
+    var foregroundSize: Float,
+    var foregroundMoveX: Int,
+    var foregroundMoveY: Int,
+    var foregroundRotate: Float,
+    var foregroundStartColor: String,
+    var foregroundEndColor: String,
+)
