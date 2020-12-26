4f412d987de18a30c2d05a688e4635563ce52783
oddlyspaced <oddlyspaced1@gmail.com>
2020-12-27 02:28:23 +0530

    HomeActivity: Init

---
+++ app/src/main/java/com/oddlyspaced/prjkt/activity/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.oddlyspaced.prjkt.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oddlyspaced.prjkt.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
