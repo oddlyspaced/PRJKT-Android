e7240d9c39df5cfa90a46311cf9cbc5e32416277
oddlyspaced <oddlyspaced1@gmail.com>
2020-12-28 20:39:10 +0530

    HomeActivity: Setup basic infinite scrolling recycler view

--- app/src/main/java/com/oddlyspaced/prjkt/activity/HomeActivity.kt
+++ app/src/main/java/com/oddlyspaced/prjkt/activity/HomeActivity.kt
@@ -2,11 +2,27 @@ package com.oddlyspaced.prjkt.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bekawestberg.loopinglayout.library.LoopingLayoutManager
 import com.oddlyspaced.prjkt.R
+import com.oddlyspaced.prjkt.adapter.EyeCandyAdapter
+import com.oddlyspaced.prjkt.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvInfinite.apply {
+            setHasFixedSize(true)
+            layoutManager = LoopingLayoutManager(
+                applicationContext, LoopingLayoutManager.VERTICAL,
+                false,
+            )
+            adapter = EyeCandyAdapter(arrayListOf("fiest", "second", "third"))
+        }
     }
 }
