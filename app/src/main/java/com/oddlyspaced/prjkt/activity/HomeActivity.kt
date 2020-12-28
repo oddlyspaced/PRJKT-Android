74faedd75c328565f4b8e61571d227f2b7a9999e
oddlyspaced <oddlyspaced1@gmail.com>
2020-12-28 20:57:20 +0530

    HomeActivity: Setup visually infinite scrolling list

--- app/src/main/java/com/oddlyspaced/prjkt/activity/HomeActivity.kt
+++ app/src/main/java/com/oddlyspaced/prjkt/activity/HomeActivity.kt
@@ -2,8 +2,10 @@ package com.oddlyspaced.prjkt.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bekawestberg.loopinglayout.library.LoopingLayoutManager
-import com.oddlyspaced.prjkt.R
 import com.oddlyspaced.prjkt.adapter.EyeCandyAdapter
 import com.oddlyspaced.prjkt.databinding.ActivityHomeBinding
 
@@ -11,18 +13,46 @@ class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
 
+    private lateinit var adapt: EyeCandyAdapter
+    private lateinit var lm: LinearLayoutManager
+    private val list = arrayListOf("1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5","1", "2,", "3,", "4", "5")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        lm = LinearLayoutManager(applicationContext)
+
+        adapt = EyeCandyAdapter(list)
+
         binding.rvInfinite.apply {
             setHasFixedSize(true)
-            layoutManager = LoopingLayoutManager(
-                applicationContext, LoopingLayoutManager.VERTICAL,
-                false,
-            )
-            adapter = EyeCandyAdapter(arrayListOf("fiest", "second", "third"))
+            layoutManager = lm
+            adapter = adapt
         }
+
+        infiniteScroll()
+    }
+
+
+    private var current = 0
+
+    private fun infiniteScroll() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            lm.scrollToPosition(
+              when {
+                  (current == list.size) -> {
+                      current = 0
+                      0
+                  }
+                  else -> {
+                      current
+                  }
+              }
+            )
+            current++
+            infiniteScroll()
+        }, 100)
     }
 }
