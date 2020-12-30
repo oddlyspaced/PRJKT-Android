d0b67c907c0a01bd55a2fa9c05a55d7e48cdecf8
oddlyspaced <oddlyspaced1@gmail.com>
2020-12-31 02:03:07 +0530

    HomeActivity: Make status bar and navigation bar transparent

--- app/src/main/java/com/oddlyspaced/prjkt/activity/HomeActivity.kt
+++ app/src/main/java/com/oddlyspaced/prjkt/activity/HomeActivity.kt
@@ -1,12 +1,24 @@
 package com.oddlyspaced.prjkt.activity
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import com.oddlyspaced.prjkt.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        this.window.apply {
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            statusBarColor = Color.TRANSPARENT
+            navigationBarColor = Color.TRANSPARENT
+        }
     }
 }
